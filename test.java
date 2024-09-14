8bcc174a5b8e2f94689daa911aa1a163ff1138ee
Jakkapp_G <141209049+jakkappagodi9@users.noreply.github.com>
2024-09-14 12:02:29 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,52 @@
+public class CircularQueue {
+    static  int[] q ;
+    static  int front, rear ,N;
+    CircularQueue(int k)
+    {
+        this.front = -1;
+        this.rear = -1;
+        this.N = k;
+        q = new int[k];
+    }
+    public static boolean enQueue(int ele)
+    {
+        if(front == -1 && rear == -1 )
+        {
+            front++;
+            rear++;
+            q[rear] = ele;
+            return true;
+        } else if (((rear + 1)%N) == front) {
+            System.out.println("Q is full");
+            return false;
+        }
+        else {
+            rear = (rear+1 )%N;
+            q[rear] = ele;
+            return true;
+        }
+    }
+    public boolean deQueue()
+    {
+        if(rear == -1 && front == -1)
+        {
+            return false;
+
+        }else if(front == rear) {
+            rear = -1;
+            front = -1;
+            return  true;
+        }else {
+            front = ((front+1)%N);
+            return true;
+        }
+    }
+    public boolean isEmp()
+    {
+        if(rear == -1 && front ==-1 )
+        {
+            return true;
+        }
+        return false;
+    }
+}
